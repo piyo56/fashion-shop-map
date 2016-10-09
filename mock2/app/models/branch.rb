34096743135b48f3ba8d25a56c89b215a5dc9806
piyo56 <piyo56.net@gmail.com>
2016-10-09 17:22:03 +0900

    [Update] 残骸を残すための（ほとんど空の）コミット

--- mock2/app/models/branch.rb
+++ mock2/app/models/branch.rb
@@ -1,7 +1,28 @@
+# class LatLngValidator < ActiveModel::Validator
+#   def validate(record)
+#     lat = record.latitude
+#     lng = record.longitude
+#     delta = 30 / 1000000
+#
+#     branches = Branch.all.select(:latitude, :longitude)
+#     branches.each do |b|
+#       if lat == b[:latitude] && lng == b[:longitude]
+#         record.latitude += delta
+#       end
+#     end
+#   end
+# end
+
 class Branch < ActiveRecord::Base
   belongs_to :shop, counter_cache: true
   belongs_to :prefecture
 
+  validates :name, presence: true, uniqueness: true
+
+  # include ActiveModel::Validations
+  # validates_with LatLngValidator
+  
   geocoded_by :address
   after_validation :geocode
+
 end
--- mock2/app/models/shop.rb
+++ mock2/app/models/shop.rb
@@ -1,3 +1,4 @@
 class Shop < ActiveRecord::Base
   has_many :branches
+  validates :name, presence: true, uniqueness: true
 end
