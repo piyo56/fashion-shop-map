f730cead52a7162904c703c2ce9160fc76280a05
piyo56 <piyo56.net@gmail.com>
2016-10-16 22:01:32 +0900

    [Add] EDIFICEのデータを追加

---
+++ mock2/db/branches/edifice/seeds.rb
@@ -0,0 +1,34 @@
+prefecture_id = Prefecture.find_by(name: "宮城県").id
+Shop.find_by(name: "EDIFICE").branches.create(name: "EDIFICE 仙台店（Le Dome）", address:"宮城県仙台市青葉区中央1-1-1", prefecture_id: prefecture_id)
+prefecture_id = Prefecture.find_by(name: "東京都").id
+Shop.find_by(name: "EDIFICE").branches.create(name: "EDIFICE TOKYO", address:"東京都渋谷区神宮前6-23-3", prefecture_id: prefecture_id)
+prefecture_id = Prefecture.find_by(name: "東京都").id
+Shop.find_by(name: "EDIFICE").branches.create(name: "EDIFICE 新宿店", address:"東京都新宿区新宿3-31-91", prefecture_id: prefecture_id)
+prefecture_id = Prefecture.find_by(name: "東京都").id
+Shop.find_by(name: "EDIFICE").branches.create(name: "EDIFICE LA BOUCLE (NEWoMan新宿店）", address:"東京都新宿区新宿4-1-6", prefecture_id: prefecture_id)
+prefecture_id = Prefecture.find_by(name: "東京都").id
+Shop.find_by(name: "EDIFICE").branches.create(name: "EDIFICE 池袋店", address:"東京都豊島区南池袋1-28-2", prefecture_id: prefecture_id)
+prefecture_id = Prefecture.find_by(name: "東京都").id
+Shop.find_by(name: "EDIFICE").branches.create(name: "EDIFICE 銀座店（Le Dome）", address:"東京都中央区銀座2-4-6", prefecture_id: prefecture_id)
+prefecture_id = Prefecture.find_by(name: "東京都").id
+Shop.find_by(name: "EDIFICE").branches.create(name: "EDIFICE 丸の内店（Le Dome）", address:"東京都千代田区丸の内2-5-2", prefecture_id: prefecture_id)
+prefecture_id = Prefecture.find_by(name: "神奈川県").id
+Shop.find_by(name: "EDIFICE").branches.create(name: "EDIFICE ルミネ横浜店（Le Dome）", address:"神奈川県横浜市西区高島2-16-1", prefecture_id: prefecture_id)
+prefecture_id = Prefecture.find_by(name: "愛知県").id
+Shop.find_by(name: "EDIFICE").branches.create(name: "EDIFICE 名古屋パルコ店（Le Dome）", address:"愛知県名古屋市中区栄3-29-1", prefecture_id: prefecture_id)
+prefecture_id = Prefecture.find_by(name: "京都府").id
+Shop.find_by(name: "EDIFICE").branches.create(name: "EDIFICE 京都店（Le Dome）", address:"京都府京都市下京区四条通寺町東入2丁目御旅町39番1号", prefecture_id: prefecture_id)
+prefecture_id = Prefecture.find_by(name: "大阪府").id
+Shop.find_by(name: "EDIFICE").branches.create(name: "EDIFICE なんばパークス店（Le Dome）", address:"大阪府大阪市浪速区難波中2-10-70", prefecture_id: prefecture_id)
+prefecture_id = Prefecture.find_by(name: "大阪府").id
+Shop.find_by(name: "EDIFICE").branches.create(name: "EDIFICE ルクア大阪店（Le Dome）", address:"大阪府大阪市北区梅田3-1-3", prefecture_id: prefecture_id)
+prefecture_id = Prefecture.find_by(name: "兵庫県").id
+Shop.find_by(name: "EDIFICE").branches.create(name: "EDIFICE 神戸店（Le Dome）", address:"兵庫県神戸市中央区雲井通7-1-1", prefecture_id: prefecture_id)
+prefecture_id = Prefecture.find_by(name: "福岡県").id
+Shop.find_by(name: "EDIFICE").branches.create(name: "EDIFICE 天神イムズ店（Le Dome）", address:"福岡県福岡市中央区天神1-7-11", prefecture_id: prefecture_id)
+prefecture_id = Prefecture.find_by(name: "広島県").id
+Shop.find_by(name: "EDIFICE").branches.create(name: "EDIFICE 広島店", address:"広島県広島市中区新天地2-1", prefecture_id: prefecture_id)
+prefecture_id = Prefecture.find_by(name: "福岡県").id
+Shop.find_by(name: "EDIFICE").branches.create(name: "EDIFICE 博多店（Le Dome）", address:"福岡県福岡市博多区博多駅中央街1-1", prefecture_id: prefecture_id)
+prefecture_id = Prefecture.find_by(name: "熊本県").id
+Shop.find_by(name: "EDIFICE").branches.create(name: "EDIFICE 熊本店（Le Dome）", address:"熊本県熊本市中央区手取本町8−1", prefecture_id: prefecture_id)
