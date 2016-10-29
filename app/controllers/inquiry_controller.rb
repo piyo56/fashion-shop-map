59af8da23dad3251fe55b90a69c6c0ab5b727e74
piyo56 <piyo56.net@gmail.com>
2016-10-29 20:04:01 +0900

    [Update] お問い合わせ機能に関わるセンシティブな情報を隠蔽

--- app/controllers/inquiry_controller.rb
+++ app/controllers/inquiry_controller.rb
@@ -20,7 +20,7 @@ class InquiryController < ApplicationController
   def thanks
     # メール送信
     @inquiry = Inquiry.new(params[:inquiry])
-    InquiryMailer.received_email(@inquiry).deliver
+    InquiryMailer.received_email(@inquiry).deliver_now
  
     # 完了画面を表示
     render :action => 'thanks'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -86,4 +86,16 @@ Rails.application.configure do
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
 
+  # Raises error for missing translations
+  # config.action_view.raise_on_missing_translations = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:               'smtp.gmail.com',
+    port:      	           587,
+    domain:                'gmail.com',
+    user_name:             'piyo56.net',
+    password:              ENV["MAIL_PASSWORD"],
+    authentication:        'plain',
+    enable_starttls_auto:  true
+  }
 end
