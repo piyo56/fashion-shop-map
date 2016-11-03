cf96226dbcbcd77b63ba80e61008f4fc270c93d8
piyo56 <piyo56.net@gmail.com>
2016-11-03 12:42:43 +0900

    [Update] ページ全体の高さを可変に、また条件ラベルの表示を改良

---
+++ public/assets/choices-8402dbc7ea21ac54c1d9478aabaf7faa2ac147efbe82defc3a92730d290a5f50.js
@@ -0,0 +1,44 @@
+// 検索ボタンの有効／無効化
+$(document).ready(function(){
+  enable_search();
+});
+enable_search = function(){
+  if ($('input[name="s_ids[]"]:checked').length === 0 ||
+      $('input[name="p_ids[]"]:checked').length === 0){
+    $("#search_btn").prop("disabled", true);
+  }else{
+    $("#search_btn").prop("disabled", false);
+  }
+  setTimeout(enable_search, 200)
+};
+
+// チェックボックスの選択処理
+$('input[name="s_ids[]"]').on('click', function(e) {
+  //イベントバブリングを防止してチェック属性が正常に切り替わるようにする
+  e.stopPropagation();
+});
+
+// 全選択／全解除処理
+$('#select_all').on('click', function(e){
+  //イベントバブリングを防止してチェック属性が正常に切り替わるようにする
+  e.stopPropagation();
+  
+  all_checked = true;
+  $('input[name="s_ids[]"]').each(function(){
+    if(!$(this).prop('checked')){
+      all_checked = false;
+      return false;
+    }
+  });
+  
+  $('input[name="s_ids[]"]').each(function(){
+    $(this).prop('checked', !all_checked);
+  });
+
+});
+$('.dropdown-menu a').on('click', function() {
+  $(this).parent().toggleClass('open');
+  var checkbox = $(this).find('input')
+  checkbox.prop("checked", !checkbox.prop("checked"));
+  return false;
+});
