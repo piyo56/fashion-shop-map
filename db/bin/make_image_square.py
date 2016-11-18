3b33fed4f45d5b832438b59edcaac2a1ddd10617
piyo56 <piyo56.net@gmail.com>
2016-11-18 10:03:02 +0900

    [Add] マーカーを作るスクリプトを２つに分離

---
+++ db/bin/make_image_square.py
@@ -0,0 +1,42 @@
+# coding: utf8
+import sys, os
+import math
+from PIL import Image
+
+"""
+与えられた画像を長辺に合わせて正方形にする
+"""
+if not len(sys.argv) == 2:
+    print("invalid argument!")
+    sys.exit()
+
+# 既存ファイルを readモードで読み込み
+logo_img = Image.open(sys.argv[1])
+if logo_img.mode != "RGBA":
+    logo_img = logo_img.convert("RGBA") # RGBモードに変換する
+
+width  = logo_img.size[0]
+height = logo_img.size[1]
+
+# 画素値をロードしてロゴの代表色をピック
+data = logo_img.load()
+logo_color = data[3,3]
+
+# もし画像が正方形でない場合は長辺に合わせて正方形にする
+if width != height:
+    length = width > height and width or height
+    marker_img = Image.new("RGBA", (length, length), logo_color)
+
+    if width > height:
+        left_top_y = math.floor((width - height) / 2)
+        paste_box = (0, left_top_y)
+    else:
+        left_top_x = math.floor((height - width) / 2)
+        paste_box = (left_top_x, 0)
+
+    marker_img.paste(logo_img, paste_box)
+else:
+    marker_img = logo_img.copy()
+
+# 保存
+marker_img.save("/Users/naka/Desktop/square.png", 'PNG', quality=100, optimize=True)
--- db/bin/make_marker_image.py
+++ db/bin/make_marker_image.py
@@ -5,12 +5,13 @@ from PIL import Image
 
 """
 与えられた画像をgmapのmarker用に整形する
+読み込み画像は正方形であると仮定
 """
 if not len(sys.argv) == 2:
     print("invalid argument!")
     sys.exit()
 
-# 既存ファイルを readモードで読み込み
+# 読み込み
 logo_img = Image.open(sys.argv[1], 'r')
 if logo_img.mode != "RGBA":
     logo_img = logo_img.convert("RGBA") # RGBモードに変換する
@@ -19,29 +20,13 @@ basename = sys.argv[1].split("/")[-1].split(".")[0]
 width  = logo_img.size[0]
 height = logo_img.size[1]
 
+# 32x32にリサイズ
+if width != 32:
+    logo_img.thumbnail((32, 32), Image.ANTIALIAS)
+
 # 画素値をロードしてロゴの代表色をピック
 data = logo_img.load()
 logo_color = data[3,3]
-
-# もし画像が正方形でない場合は長辺に合わせて正方形にする
-if width != height:
-    length = width > height and width or height
-    marker_img = Image.new("RGBA", (length, length), logo_color)
-
-    if width > height:
-        left_top_y = math.floor((width - height) / 2)
-        paste_box = (0, left_top_y)
-    else:
-        left_top_x = math.floor((height - width) / 2)
-        paste_box = (left_top_x, 0)
-
-    marker_img.paste(img, paste_box)
-else:
-    marker_img = logo_img.copy()
-
-# 30x30にリサイズして外側に黒枠線を付ける。(32x32)
-if width != 32:
-    marker_img.thumbnail((32, 32), Image.ANTIALIAS)
 
 # ピン用の下三角画像を読み込み
 pin_img_path = os.environ.get("PROJECT_HOME") + "/app/assets/images/pin.png"
@@ -59,4 +44,5 @@ marker_pin_img = Image.new("RGBA", (32, 44), (0, 0, 0, 0))
 marker_pin_img.paste(marker_img, (0, 0))
 marker_pin_img.paste(pin_img, (8, 32))
 
+# 保存
 marker_pin_img.save("{}/app/assets/images/{}.png".format(os.environ["PROJECT_HOME"], basename), 'PNG', quality=100, optimize=True)
