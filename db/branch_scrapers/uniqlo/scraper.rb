a4f2af89bc2bb1f09a892f7d6410f9df3fb42f45
piyo56 <piyo56.net@gmail.com>
2016-11-28 01:02:28 +0900

    [Fix] ユニクロの店舗が一部の店舗のみ登録されていたため、全て登録

--- db/branch_scrapers/uniqlo/scraper.rb
+++ db/branch_scrapers/uniqlo/scraper.rb
@@ -8,12 +8,15 @@ def fetch_page(url)
   charset = nil
   html = open(url) do |f|
     raise "Got 404 error" if f.status[0] == "404"
-
     charset = f.charset # 文字種別を取得
     f.read # htmlを読み込んで変数htmlに渡す
   end
+  
+  html = html.toutf8.gsub!(/<script[^>]*>.*?<\/script>/m, "")
+  doc = Nokogiri::HTML.parse(html, nil, 'utf-8') do |config|
+   config.noblanks.strict.nonet
+  end
 
-  doc = Nokogiri::HTML.parse(html.toutf8, nil, 'utf-8')
   return doc
 end
 
@@ -28,37 +31,38 @@ end
 
 # スクレイピング先の情報
 shop_name       = "UNIQLO"
-short_shop_name = "UNIQLO"
-target_url      = "http://www.uniqlo.com/jp/shop/"
-
+prefecutres = ["北海道", "青森県", "秋田県", "岩手県", "山形県", "宮城県", "新潟県", "福島県", "富山県", "石川県", "福井県", "茨城県", "栃木県", "群馬県", "長野県", "千葉県", "埼玉県", "東京都", "神奈川県", "山梨県", "静岡県", "愛知県", "岐阜県", "三重県", "滋賀県", "大阪府", "京都府", "奈良県", "和歌山県", "兵庫県", "鳥取県", "島根県", "岡山県", "広島県", "山口県", "香川県", "徳島県", "高知県", "愛媛県", "福岡県", "佐賀県", "長崎県", "熊本県", "大分県", "宮崎県", "鹿児島県", "沖縄県"]
+  
 File.open("seeds.rb", "w") do |file|
-  # 店舗検索TOPページ取得
-  top_page = fetch_page(target_url)
 
-  top_page.css("area").each do |prefecture_dom|
-    branches = []
-    branch_prefecture = prefecture_dom.attribute("alt").value
-    branch_list_url   = prefecture_dom.attribute("href").value
-    code = %(prefecture_id = Prefecture.find_by(name: "#{branch_prefecture}").id\n)
+  # 各都道府県の店舗をスクレイピング
+  prefecutres.each_with_index do |prefecutre_name, prefecutre_id|
 
-    print "#{branch_prefecture}: "
+    seeds_code  = %(prefecture_id = Prefecture.find_by(name: "#{prefecutre_name}").id\n)
+    branch_list_url = "http://www.uniqlo.com/jp/shop/c/all/index_#{prefecutre_id + 1}.html"
 
-    # 各都道府県のページを取得
+    # 店舗情報取得
     branch_list_page = fetch_page(branch_list_url)
-    branch_list_page.css('#contents > table tr').each do |row|
-      branch_name    = row.css(".tit").inner_text.strip
-      branch_address = row.css(".txt").inner_text.split("\n")[0].strip
-      
-      print "*"
-      #puts "\t* #{branch_name} | #{branch_address}"
+    puts "#{prefecutre_name}: #{branch_list_page.css("#contents table tr").length}"
+    branch_list_page.css("table tr").each do |row|
 
-      # 配列に保存
-      branches.push({name: branch_name, url: target_url, address: branch_address.split(" ")[0], long_address: branch_address, prefecture: branch_prefecture})
+      # 名前と住所
+      branch_name    = row.css(".tit").inner_text.strip
+      branch_address = row.css(".txt").inner_text.split("\n")[0]
+      
+      # なぜか一行ごとに空のカラムが入っているのでその場合はスルー
+      if branch_name.empty?
+        next
+      end
+
+      # 閉店(した|する)場合はスキップ
+      if branch_name.include?("閉店")
+        next
+      end
 
       # seedsに書き出し
-      code += %(Shop.find_by(name: "#{shop_name}").branches.create(name: "#{branch_name}", address:"#{branch_address}", prefecture_id: prefecture_id)\n)
+      seeds_code += %(Shop.find_by(name: "#{shop_name}").branches.create(name: "#{branch_name}", address:"#{branch_address}", prefecture_id: prefecture_id)\n)
     end
-    print "\n"
-    file.write(code)
+    file.write(seeds_code)
   end
 end
