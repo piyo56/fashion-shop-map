cba56198d5b3ae7d0c9d33b161e8a5bbec166ce6
piyo56 <piyo56.net@gmail.com>
2017-08-13 13:53:08 +0900

    :sparkles: update deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,3 +14,11 @@ set :rbenv_ruby, '2.3.1'
 set :rbenv_path, '~/.rbenv'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+
+namespace :deploy do
+  desc 'Restart application'
+  task :restart do
+    invoke 'unicorn:restart' #lib/capustrano/tasks/unicorn.cap内処理を実行
+  end
+  after :finishing, 'deploy:cleanup'
+end
